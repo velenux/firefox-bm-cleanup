6682e07fac919d163c82c9ae955f8830e861a185
Gilberto Ficara <g.ficara@stardata.it>
2016-12-13 21:13:01 +0100

    Cleanup: moved the handling of containers and bookmarks in different functions

--- ff-bm-cleanup.py
+++ ff-bm-cleanup.py
@@ -91,6 +91,7 @@ def normalize_tags(bm_tags, page_tags):
 
 def remove_entry(container, entry):
 	"""Try to remove an entry from a container."""
+	# http://stackoverflow.com/questions/18418/elegant-way-to-remove-items-from-sequence-in-python
 	logging.debug('[x] ID: %s removing entry', entry['id'])
 	try:
 		container['children'].remove(entry)
@@ -98,76 +99,85 @@ def remove_entry(container, entry):
 		logging.warn('Error removing entry: %s', e)
 
 
+def entry_find_name(entry):
+	"""Find a valid name for the entry."""
+	# FIXME: could make it better
+	name = 'Untitled entry'
+	if 'title' in entry and entry['title'] != '':
+		name = normalize_string(entry['title'])
+	return name
+
+
+def entry_handle_container(container, entry, path, name):
+	"""Handle a container entry."""
+	logging.debug('[ ] ID: %s is a container', entry['id'])
+	# short-circuit if the container is empty
+	if not 'children' in entry or len(entry['children']) == 0:
+		logging.debug('[ ] ID: %s container is empty, removing', entry['id'])
+		remove_entry(container, entry)
+		return
+	path.append(name) # add the name to the path stack
+	iterate_bookmarks(path, entry) # iterate over the entry
+	del path[-1] # remove the name from the path stack when you're done
+
+
+def entry_handle_bookmark(container, entry, path, name):
+	"""Handle a bookmark entry."""
+	logging.debug('uri ID: %s is a bookmark (%s)', entry['id'], entry['uri'])
+	page_metadata = {
+		'title': '',
+		'description': '',
+		'keywords': ''
+	}
+	# short-circuit check if the uri is duplicate
+	# FIXME: using global variables is ugly
+	if entry['uri'] in URIS:
+		logging.debug('[x] ID: %s has a duplicate URI, removing', entry['id'])
+		remove_entry(container, entry)
+		return
+	# if the uri is not in the list, add it
+	# FIXME: using global variables is ugly
+	URIS.add(entry['uri'])
+	# try to fetch the page
+	if entry['uri'].startswith('http://') or entry['uri'].startswith('https://'):
+		try:
+			req = http_req(entry['uri'])
+			if req.status_code == 200:
+				if len(req.history) > 0:
+					# update the uri if it has been changed (redirects, etc)
+					logging.warn('-m- ID: %s url changed old(%s) new(%s)', entry['id'], entry['uri'], req.url)
+					entry['uri'] = req.url
+				page_metadata = get_page_metadata(req.text)
+			else:
+				logging.error('-x- ID: %s bad return code for %s', entry['id'], entry['url'])
+				remove_entry(container, entry)
+				return
+		except Exception as e:
+			logging.error('-x- ID: %s exception while fetching (%s): %s', entry['id'], entry['url'], e)
+			remove_entry(container, entry)
+			return
+	# normalize tags
+	if not 'tags' in entry:
+		entry['tags'] = '' 	# add an empty tag list
+	entry['tags'] = normalize_tags(entry['tags'], page_metadata['keywords'])
+	# report
+	logging.info('%s/%s | ID: %s | URI: %s | Tags: %s', \
+		'/'.join(path), name, entry['id'], entry['uri'], entry['tags'])
+	print("{0} / {1} | ID: {2} | URI: {3} | Tags: {4}".format(\
+		'/'.join(path), name, entry['id'], entry['uri'], entry['tags']))
+
+
 def iterate_bookmarks(path, container):
 	"""Iterate over the bookmark tree."""
 	# report where I am
 	logging.debug('>>> ID: %s iterate_bookmarks(%s, %s [...])', container['id'], path, str(container)[0:30])
-	# iterate over the childrens
-	# http://stackoverflow.com/questions/18418/elegant-way-to-remove-items-from-sequence-in-python
-	logging.debug('[ ] ID: %s, %s items to process', container['id'], len(container['children']))
 	for entry in reversed(container['children']):
-		# find a preliminary name
-		name = 'Untitled entry'
-		if 'title' in entry and entry['title'] != '':
-			name = normalize_string(entry['title'])
-		logging.debug('<o> ID: %s (%s)', entry['id'], name)
-		# add an empty tag list
-		if not 'tags' in entry:
-			entry['tags'] = ''
-		# if it is a container, recurse
+		name = entry_find_name(entry)
+		logging.debug('--> ID: %s %s/%s', entry['id'], path, name)
 		if entry['type'] == 'text/x-moz-place-container':
-			# short-circuit if the container is empty
-			if not 'children' in entry or len(entry['children']) == 0:
-				logging.debug('[ ] ID: %s container is empty', entry['id'])
-				remove_entry(container, entry)
-				return
-			logging.debug('[ ] ID: %s is a container', entry['id'])
-			path.append(name) # add the name to the path stack
-			iterate_bookmarks(path, entry) # iterate over the entry
-			del path[-1] # remove the name from the path stack when you're done
-		# if the entry is a location
+			entry_handle_container(container, entry, path, name)
 		if entry['type'] == 'text/x-moz-place':
-			logging.debug('uri ID: %s [%s]', entry['id'], entry['uri'])
-			status = True  # we assume the bookmark is ok
-			page_metadata = {
-				'title': '',
-				'description': '',
-				'keywords': ''
-			}
-			# short-circuit check if the uri is duplicate
-			# FIXME: using global variables is ugly
-			if entry['uri'] in URIS:
-				logging.debug('[x] ID: %s has a duplicate URI, removing', entry['id'])
-				remove_entry(container, entry)
-				continue
-			# if the uri is not in the list, add it
-			# FIXME: using global variables is ugly
-			URIS.add(entry['uri'])
-			# try to fetch the page
-			if entry['uri'].startswith('http://') or entry['uri'].startswith('https://'):
-				try:
-					req = http_req(entry['uri'])
-					if req.status_code == 200:
-						if len(req.history) > 0:
-							# update the uri if it has been changed (redirects, etc)
-							logging.warn('mMm ID: %s url changed old(%s) new(%s)', entry['id'], entry['uri'], req.url)
-							entry['uri'] = req.url
-						page_metadata = get_page_metadata(req.text)
-					else:
-						status = False
-				except Exception as e:
-					logging.error('FAIL while getting [%s]: %s', entry['uri'], e)
-					status = False
-			if status == False:
-				logging.warn('[x] ID: %s status FAIL, removing', entry['id'])
-				remove_entry(container, entry)
-			# normalize tags
-			entry['tags'] = normalize_tags(entry['tags'], page_metadata['keywords'])
-			# report
-			logging.info('%s/%s | ID: %s | URI: %s | Tags: %s', \
-				'/'.join(path), name, entry['id'], entry['uri'], entry['tags'])
-			print("{0} / {1} | ID: {2} | URI: {3} | Tags: {4}".format(\
-				'/'.join(path), name, entry['id'], entry['uri'], entry['tags']))
+			entry_handle_bookmark(container, entry, path, name)
 
 
 # ~~~ main ~~~
